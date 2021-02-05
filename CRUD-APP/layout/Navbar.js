a0db938bdea900e01c77aba94bf58e9108c89881
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-02-05 11:38:41 +0530

    Update Navbar.js

--- CRUD-APP/layout/Navbar.js
+++ CRUD-APP/layout/Navbar.js
@@ -7,28 +7,22 @@ const Navbar = ()=>
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-info">
       <div className="Container">
-         
-      
            <div className="collapse navbar-collapse">
             <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" exact to="/">Home 
-              </NavLink>
+              <NavLink className="nav-link" exact to="/">Home</NavLink>
             </li>
             <li className="nav-item">
               <NavLink className="nav-link" exact to="/about">About</NavLink>
             </li>
-            
             <li className="nav-item">
               <NavLink className="nav-link" exact to="/contact">Contact</NavLink>
             </li>
           </ul>
           <Link className="btn btn-outline-light" to="/user/add">Add User</Link>
         </div>
-        
-        </div>
-      </nav>
-     
+       </div>
+    </nav> 
     );
 };
 
