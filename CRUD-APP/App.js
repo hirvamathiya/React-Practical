e340b62693e1e58eb7690e1220253796d1e9d326
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-02-05 14:38:09 +0530

    Updated App.js

--- CRUD-APP/App.js
+++ CRUD-APP/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React,{Suspense} from 'react';
+import Loader from '../../Components/Loader/LoaderModal';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
 import Navbar from './layout/Navbar';
-import Error from './components/Error';
+// import Error from './components/Error';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import AddUser from './user/AddUser';
-import EditUser from './user/EditUser';
-import ViewUser from './user/ViewUser';
+// import AddUser from './user/AddUser';
+// import EditUser from './user/EditUser';
+// import ViewUser from './user/ViewUser';
+ const Home = React.lazy(() => import('./components/Home'));
+ const About = React.lazy(() => import('./components/About'));
+ const Contact = React.lazy(() => import('./components/Contact'));
+ const Error = React.lazy(() => import('./components/Error'));
+ const AddUser = React.lazy(() => import('./user/AddUser'));
+ const EditUser = React.lazy(() => import('./user/EditUser'));
+ const ViewUser = React.lazy(() => import('./user/ViewUser'));
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
         <Navbar />
+        <Suspense fallback={<div>Loading...</div>}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
@@ -25,6 +34,7 @@ function App() {
           <Route exact path="/user/view/:id" component={ViewUser} />
           <Route component={Error} />
         </Switch>
+        </Suspense>
     </div>
     </BrowserRouter>
   );
