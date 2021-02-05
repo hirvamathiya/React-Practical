b9c9141e0abbff6d09c175ace3fa5c301a65409b
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-02-05 11:33:42 +0530

    Removed ] from the code.

--- CRUD-APP/component/Error.js
+++ CRUD-APP/component/Error.js
@@ -3,9 +3,8 @@ import React from 'react';
 const Error = () =>{
 
     return(
-   
           <div className="errorMsg">
-              ]<h1 className="display-1">Oops !! Not Found</h1>
+           <h1 className="display-1">Oops !! Not Found</h1>
           </div>
     );
 }
