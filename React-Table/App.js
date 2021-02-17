61300f185a3a4e01c8e571134326ba68171570e2
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-02-17 17:41:28 +0530

    Add files via upload

---
+++ React-Table/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+//import { FilteringTable } from './components/FilteringTable';
+import { PaginationTable } from './components/PaginationTable';
+//import { BasicTable } from './components/BasicTable';
+//import { SortingTable } from './components/SortingTable';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <BasicTable /> */}
+       {/* <SortingTable /> */}
+       {/* <FilteringTable /> */}
+       <PaginationTable />
+    </div>
+  );
+}
+
+export default App;
