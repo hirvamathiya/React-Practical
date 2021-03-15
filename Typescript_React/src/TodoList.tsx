a83c9bd23da398992bb6dcf96e5f4674168bef92
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-03-15 17:57:36 +0530

    Removed {} and return .

--- Typescript_React/src/TodoList.tsx
+++ Typescript_React/src/TodoList.tsx
@@ -8,7 +8,7 @@ interface TodoListProps{
 }
 
 export const TodoList:React.FC<TodoListProps> = ({ todos,toggleTodo}) =>{
-     return <ul>{todos.map(todo =>{
-        return <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo}/>
-     })}</ul>
+     return <ul>{todos.map(todo =>
+         <TodoListItem key={todo.text} todo={todo} toggleTodo={toggleTodo}/>
+     )}</ul>
 }
