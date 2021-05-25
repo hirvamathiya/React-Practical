3149fcf55c6e655f780c4fe6cb16e29098e5f477
hirvamathiya <70806891+hirvamathiya@users.noreply.github.com>
2021-05-25 18:46:44 +0530

    Add files via upload

---
+++ MicroFrontend-Tutorial/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import GreetingCat from "./GreetingCat";
+import RandomCat from "./RandomCat";
+
+const defaultHistory = createBrowserHistory();
+
+function App({ history = defaultHistory }) {
+  return (
+    <Router history={history}>
+      <Route exact path="/" component={RandomCat} />
+      <Route exact path="/cat/:greeting" component={GreetingCat} />
+    </Router>
+  );
+}
+
+
+export default App;
